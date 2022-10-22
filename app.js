1ed91b7953f86d9538e01895364f41ea956fbddf
Blessings Fatch <83654280+blessed-byte@users.noreply.github.com>
2022-10-22 14:08:41 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+const attrs = ['blessings', 'fatch', 'kanjedza', 'computer' ]
+console.log(attrs);
+const attrsObject = {}
+for (let i = 0; i < attrs.length; i++) {
+    attrsObject.name = attrs[i]
+    console.log(attrsObject)
+}
+
+// for (let i = 0; i < numbers.length; i++) {
+//     //To find even numbers
+//     if (numbers[i] % 2 !== 0) continue;
+//     console.log(numbers[i]);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//     //To find odd numbers
+//     if (numbers[i] % 2 !== 0) console.log(numbers[i]);;
+    
+// }
